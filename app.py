c8600259fe08db76f4858dd5e826289f85f4f648
rishivyas1969 <rishivyas2424@gmail.com>
2021-09-05 00:43:29 +0530

    completed

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ import difflib
 data = json.load(open('data.json'))
 str = input("\nEnter a word: ")
 
+print("\b", end='')
 def findWord(w):
     w = w.lower()
-    print(w)
     if w in data.keys():
         return data[w]
+
     else:
+        lst = difflib.get_close_matches(w, data.keys())
+        for j in lst:
+            ch = input("Did you mean '"+ j +"' [Y/N]:")
+            if ch == 'y' or ch == 'Y':
+                return data[j]
         return ["!!! Word doesn't exist! Please double check it. !!!"]
 
 print("\n")
